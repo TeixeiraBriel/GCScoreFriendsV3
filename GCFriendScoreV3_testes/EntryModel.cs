08da96db9783bcb628194f822dac47d62ea6a518
TeixeiraBriel <teixeirabriel@gmail.com>
2020-08-26 13:28:18 -0300

    Base do scraper feita, porém o site tem auth, então preciso logar antes de ir de fato pro site da GC com os dados.

---
+++ GCFriendScoreV3_testes/EntryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCFriendScoreV3_testes
+{
+    public class EntryModel
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ GCFriendScoreV3_testes/Scraper.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCFriendScoreV3_testes
+{
+    public class Scraper
+    {
+        private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        public ObservableCollection<EntryModel> Entries
+        {
+            get { return _entries; }
+            set { _entries = value; }
+        }
+
+        public void ScrapeData(string url)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            //ta parando na tela de login
+            var dados = doc.DocumentNode.SelectNodes("//*[@class='btn-create-account']");
+            
+            
+
+        }
+    }
+}
